d92db1f2ea61f8a9ed348e6c22dd3bcf8c19ab17
linkd2ev <m89824@ncgn.co.kr>
2018-07-17 17:14:28 +0900

    [springboot] frist commit

---
+++ springboot_v1_maven/src/main/java/com/example/demo/SpringbootV1MavenApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootV1MavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootV1MavenApplication.class, args);
+	}
+}
